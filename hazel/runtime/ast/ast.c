3043319a4f0416900f0d70b8b2f17254970ef9d7
Jason Dsouza <jasmcaus@gmail.com>
2021-03-09 13:35:06 +0530

    Update ast

--- hazel/runtime/ast/ast.c
+++ hazel/runtime/ast/ast.c
@@ -6,12 +6,20 @@
 Ast* ast_init(int type) {
     Ast* ast = calloc(1, sizeof(struct AstDef)); 
     ast->type = type; 
+    
+    // AST_VARIABLE_DEFINITION
     ast->variable_definition_variable_name = null; 
     ast->variable_definition_value = null;
+
+    // AST_VARIABLE
     ast->variable_name = null; 
+
+    // AST_FUNCTION_CALL
     ast->function_call_name = null; 
     ast->function_call_args = null; 
     ast->function_call_args_size = 0; 
+
+    // AST_STRING
     ast->string_value = null; 
 
     return ast; 
