ea0d057ded84f6616e500465e366a23ce0e99387
Jason Dsouza <jasmcaus@gmail.com>
2021-03-09 13:06:45 +0530

    runtime/types ==> types

--- hazel/hazel.h
+++ hazel/hazel.h
@@ -2,6 +2,9 @@
 
 // This ALWAYS needs to be right at the top 
 #include <hazel/core/hdef.h> 
+#include <hazel/types/types.h> 
 
 #include <hazel/runtime/lexer/lexer.h>
 #include <hazel/runtime/tokens/tokens.h> 
+#include <hazel/runtime/ast/ast.h>
+#include <hazel/runtime/parser/parser.h> 
