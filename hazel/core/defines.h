5e7d5f47836485e31c0f1711ad95d6d22a8effb1
Jason Dsouza <jasmcaus@gmail.com>
2021-03-09 13:03:12 +0530

    Update defines

--- hazel/core/defines.h
+++ hazel/core/defines.h
@@ -3,6 +3,17 @@
 #define null (void*)0
 #define nullchar '\0'
 
+// The same thing as size_t 
+#ifndef _Ull_DEFINED
+    #define _Ull_DEFINED
+    #undef Ull
+    #ifdef _WIN64
+        typedef unsigned __int64 Ull;
+    #else
+        typedef unsigned int Ull;
+    #endif //_WIN64
+#endif
+
 // bool is a basic type in C++ and not C
 // We could just have used <stdbool.h> but I prefer this -- smaller binary, easily maintanable
 #ifndef __cplusplus
