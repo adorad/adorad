31421fd27f6cb0ed9267ca73240e3b527576e639
Jason Dsouza <jasmcaus@gmail.com>
2021-04-18 23:36:40 +0530

    Add main.c

---
+++ old-hazel/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+#include <hazel/hazel.h>
+
+int main() {
+    Lexer* lexer = lexer_init(
+        "var name = \"John\" \n"
+        "print(name)\n"
+    ); 
+
+    Parser* parser = parser_init(lexer);
+    Ast* root = parser_parse(parser);
+
+    printf("%d\n", root->type);
+    printf("%d\n", root->compound_size);
+}
