3ff7b8415fc3434077318754ec82980baa537a1e
Jason Dsouza <jasmcaus@gmail.com>
2021-04-22 00:55:06 +0530

    Rename to Hazel

---
+++ Hazel/Hazel.h
@@ -0,0 +1,10 @@
+// Includes all the files needed for Hazel compilation
+
+// This ALWAYS needs to be right at the top 
+#include <Hazel/Core/HCore.h> 
+// #include <Hazel/Types/Types.h> 
+
+#include <Hazel/Compiler/Lexer/Lexer.h>
+#include <Hazel/Compiler/Tokens/Tokens.h> 
+// #include <Hazel/Compiler/Ast/Ast.h>
+// #include <Hazel/Compiler/Parser/Parser.h> 
