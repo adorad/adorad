f1088a7cec81cd391cb0f3cf2dbe590426d32804
Jason Dsouza <jasmcaus@gmail.com>
2021-04-21 17:47:55 +0530

    Update string.h

--- hazel/core/string.h
+++ hazel/core/string.h
@@ -169,32 +169,107 @@ static inline void strToUpper(char* str) {
     }
 }
 
+
+CSTL_DEF const char* strTok(char* output, char const* src, char const* delimit) {
+
+}
+
+CSTL_DEF bool strHasPrefix(char const* str, char const* prefix) {
+
+}
+
+CSTL_DEF bool strHasSuffix(char const* str, char const* suffix) {
+
+}
+
+CSTL_DEF char const* charFirstOccurence(char const* str, char c) {
+
+}
+
+CSTL_DEF char const* charLastOccurence (char const* str, char c) {
+
+}
+
+
+// The functions below are part of C's actual library. 
+// They're here as part of CSTL so that you don't have to include multiple header files (such as C's <string.h>)
+
+/* Copyright (C) 1991-2018 Free Software Foundation, Inc.
+    This file is part of the GNU C Library.
+    Written by Torbjorn Granlund (tege@sics.se),
+    with help from Dan Sahlin (dan@sics.se);
+    commentary by Jim Blandy (jimb@ai.mit.edu).
+    The GNU C Library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+    The GNU C Library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public
+    License along with the GNU C Library; if not, see
+    <http://www.gnu.org/licenses/>.  
+*/
+
 CSTL_DEF Ll strLen(const char* str) {
-    const char* begin = str; 
-    const Ll* w; 
+    const char* char_ptr;
+    const unsigned long int* longword_ptr;
+    unsigned long int longword, himagic, lomagic;
 
-    if(str == null) {
-        return 0;
+    // Handle the first few characters by reading one character at a time.
+    // Do this until CHAR_PTR is aligned on a longword boundary. 
+    for (char_ptr = str; ((unsigned long int) char_ptr & (sizeof (longword) - 1)) != 0; ++char_ptr)
+        if (*char_ptr == '\0')
+        return char_ptr - str;
+
+    // All these elucidatory comments refer to 4-byte longwords,
+    // but the theory applies equally well to 8-byte longwords.  */
+
+    longword_ptr = (unsigned long int *) char_ptr;
+
+    // Bits 31, 24, 16, and 8 of this number are zero.  Call these bits the "holes."  Note that there is a hole just to the 
+    // left of each byte, with an extra at the end: 
+    //       bits:  01111110 11111110 11111110 11111111
+    //       bytes: AAAAAAAA BBBBBBBB CCCCCCCC DDDDDDDD
+    // The 1-bits make sure that carries propagate to the next 0-bit.
+    // The 0-bits provide holes for carries to fall into. 
+    himagic = 0x80808080L;
+    lomagic = 0x01010101L;
+
+    if (sizeof(longword) > 4){
+        // 64-bit version of the magic. 
+        // Do the shift in two steps to avoid a warning if long has 32 bits.
+        himagic = ((himagic << 16) << 16) | himagic;
+        lomagic = ((lomagic << 16) << 16) | lomagic;
     }
 
-    while(cast(UIntptr)str % sizeof(Ull)) {
-        if(!*str)
-            return str - begin;
-        str++;
+    if (sizeof(longword) > 8)
+        abort();
+
+    // Instead of the traditional loop which tests each character, we will test a longword at a time. The tricky part is 
+    // testing if *any of the four* bytes in the longword in question are zero.
+    for(;;) {
+        longword = *longword_ptr++;
+
+        if (((longword - lomagic) & ~longword & himagic) != 0) {
+            // Which of the bytes was the zero?  If none of them were, it was a misfire; continue the search.
+
+            const char *cp = (const char *) (longword_ptr - 1);
+
+            if (cp[0] == 0)  return cp - str;
+            if (cp[1] == 0)  return cp - str + 1;
+            if (cp[2] == 0)  return cp - str + 2;
+            if (cp[3] == 0)  return cp - str + 3;
+
+            if (sizeof(longword) > 4) {
+                if (cp[4] == 0) return cp - str + 4;
+                if (cp[5] == 0) return cp - str + 5;
+                if (cp[6] == 0) return cp - str + 6;
+                if (cp[7] == 0) return cp - str + 7;
+            }
+        }
     }
-
-    w = cast(const Ll*)str;
-    while(!CSTL__HAS_ZERO(*w)) {
-        w++;
-    }
-
-    str = cast(const Ll*)w; 
-    while(*str) {
-        str++;
-    }
-    return str - begin; 
-
-
 }
 
 CSTL_DEF Ll strnLen(const char* str, Ll max_len) {
@@ -222,26 +297,6 @@ CSTL_DEF Ll strlCopy(char *dest, const char* source, Ll len) {
 }
 
 CSTL_DEF char* strRev(char *str) {
-
-}
-
-CSTL_DEF const char* strTok(char* output, char const* src, char const* delimit) {
-
-}
-
-CSTL_DEF bool strHasPrefix(char const* str, char const* prefix) {
-
-}
-
-CSTL_DEF bool strHasSuffix(char const* str, char const* suffix) {
-
-}
-
-CSTL_DEF char const* charFirstOccurence(char const* str, char c) {
-
-}
-
-CSTL_DEF char const* charLastOccurence (char const* str, char c) {
 
 }
 
