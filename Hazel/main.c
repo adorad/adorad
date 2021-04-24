880a26507764ae6cbf221262658ed303a338bb8a
Jason Dsouza <jasmcaus@gmail.com>
2021-04-24 14:45:11 +0530

    Few updates

--- Hazel/main.c
+++ Hazel/main.c
@@ -2,15 +2,16 @@
 
 #include <Hazel/Hazel.h>
 
-int main() {
-    // Lexer* lexer = lexer_init(
-    //     "var name = \"John\" \n"
-    //     "print(name)\n"
-    // ); 
+int main(int argc, char* arv[]) {
+    bool interactive_mode = false; 
 
-    // Parser* parser = parser_init(lexer);
-    // Ast* root = parser_parse(parser);
+    if(argc < 2) {
+        interactive_mode = true; 
+        while(interactive_mode) {
+            char* str = getStdIn("> "); 
+            Lexer* lexer = lexer_init(str); 
+        }
+    }
+    
 
-    // printf("%d\n", root->type);
-    // printf("%d\n", root->compound_size);
 }
