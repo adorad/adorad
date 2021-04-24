9a8794ccda9faf6a1cbc9ff91d8051468656b35e
Jason Dsouza <jasmcaus@gmail.com>
2021-04-25 00:13:12 +0530

    Update main

--- Hazel/main.c
+++ Hazel/main.c
@@ -3,20 +3,23 @@
 #include <Hazel/Hazel.h>
 
 int main(int argc, char* argv[]) {
-    // // Example: 
-    // // To compile a Hazel source file:
-    // // >> hazel -c hello.hzl
-    // if(argc<3) {
-    //     printf("Too few arguments"); 
-    //     return 1;
-    // }
+    // Example: 
+    // To compile a Hazel source file:
+    // >> hazel compile hello.hzl
+    if(argc<3) {
+        printf("Too few arguments"); 
+        return 1;
+    }
 
-    // if(strcmp(argv[1], "-c") == 0) {
-    //     char* source = readFile(argv[2]);
-    // }
-    char* source = readFile("test/LexerDemo.hzl");
-    printf(source);
-    free(source);
+    if(strcmp(argv[1], "compile") == 0) {
+        char* source = readFile(argv[2]);
+        
+        // Delete when done 
+        free(source); 
+    }
+    // char* source = readFile("test/LexerDemo.hzl");
+    // printf(source);
+    // free(source);
 
     return 0; 
 }
