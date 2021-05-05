dac9e9bd2d69ebafbf7d473836ff53a8ddc93c64
Jason Dsouza <jasmcaus@gmail.com>
2021-05-05 19:04:35 +0530

    Remove old-hazel

--- old-hazel/runtime/tokens/tokens.c
+++
@@ -1,7 +0,0 @@
-#include <Hazel/Compiler/Tokens/Tokens.h> 
-
-Token* token_init(int type, char* value) {
-    Token* tok = calloc(1, sizeof(struct TokenNames)); 
-    tok->type = type; 
-    tok->value = value;
-}
