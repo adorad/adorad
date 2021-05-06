3697c480f2ad60b038d2a7b2a0a344547de0952a
Jason Dsouza <jasmcaus@gmail.com>
2021-05-06 19:27:53 +0530

    Update test

--- test/Compiler/Lexer.cpp
+++ test/Compiler/Lexer.cpp
@@ -3,6 +3,6 @@ void test_Lexer_init() {
     std::string buffer = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     Hazel::Lexer lexer(buffer);
 
-    CSTL_ASSERT(lexer.get_buffer() == "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    CSTL_ASSERT(lexer.get_buffer_length() == 35);
+    CSTL_ASSERT(lexer.buffer() == "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    CSTL_ASSERT(lexer.buffer_length() == 35);
 }
