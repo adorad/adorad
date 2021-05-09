67727f3a89598107ddc296df7291647117b61ced
Jason Dsouza <61184127+jasmcaus@users.noreply.github.com>
2021-05-09 11:03:35 +0530

    Update Debug.h

--- Hazel/Core/Debug.h
+++ Hazel/Core/Debug.h
@@ -20,12 +20,12 @@ Copyright (c) 2021 Jason Dsouza <http://github.com/jasmcaus>
 
 // ========================= Debug + Asserts =========================
 
-#ifndef CSTL_CHECK
-    #define CSTL_CHECK3(cond, msg)       typedef char static_assertion_##msg[(!!(cond))*2-1]
+#ifndef CSTL_DEBUG_CHECK
+    #define CSTL_DEBUG_CHECK3(cond, msg)       typedef char static_assertion_##msg[(!!(cond))*2-1]
     // NOTE(jasmcaus): Token pasting madness!!
-    #define CSTL_CHECK2(cond, line)      CSTL_CHECK3(cond, static_assertion_at_line_##line)
-    #define CSTL_CHECK1(cond, line)      CSTL_CHECK2(cond, line)
-    #define CSTL_CHECK(cond)             CSTL_CHECK1(cond, __LINE__)
+    #define CSTL_DEBUG_CHECK2(cond, line)      CSTL_DEBUG_CHECK3(cond, static_assertion_at_line_##line)
+    #define CSTL_DEBUG_CHECK1(cond, line)      CSTL_DEBUG_CHECK2(cond, line)
+    #define CSTL_DEBUG_CHECK(cond)             CSTL_DEBUG_CHECK1(cond, __LINE__)
 #endif
 
 
