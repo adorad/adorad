57c7632317ea76c8eb480310a7504219200dc9a4
Jason Dsouza <jasmcaus@gmail.com>
2021-05-09 18:42:55 +0530

    Changes to Arrow

--- Hazel/main.c
+++ Hazel/main.c
@@ -2,82 +2,85 @@
 // #include <string>
 // #include <iostream>
 
-TEST(c, ASSERT_TRUE) { ASSERT_TRUE(1); }
+UTEST_MAIN()
 
-TEST(c, ASSERT_FALSE) { ASSERT_FALSE(0); }
+UTEST(c, ASSERT_TRUE) { ASSERT_TRUE(1); }
 
-TEST(c, ASSERT_EQ) { ASSERT_EQ(1, 1); }
+UTEST(c, ASSERT_FALSE) { ASSERT_FALSE(0); }
 
-TEST(c, ASSERT_NE) { ASSERT_NE(1, 2); }
+UTEST(c, ASSERT_EQ) { ASSERT_EQ(1, 1); }
 
-TEST(c, ASSERT_LT) { ASSERT_LT(1, 2); } 
+UTEST(c, ASSERT_NE) { ASSERT_NE(1, 2); }
 
-TEST(c, ASSERT_LE) {
+UTEST(c, ASSERT_LT) { ASSERT_LT(1, 2); } 
+
+UTEST(c, ASSERT_LE) {
   ASSERT_LE(1, 1);
   ASSERT_LE(1, 2);
 }
 
-TEST(c, ASSERT_GT) { ASSERT_GT(2, 1); }
+UTEST(c, ASSERT_GT) { ASSERT_GT(2, 1); }
 
-TEST(c, ASSERT_GE) {
+UTEST(c, ASSERT_GE) {
   ASSERT_GE(1, 1);
   ASSERT_GE(2, 1);
 }
 
-TEST(c, ASSERT_STREQ) { ASSERT_STREQ("foo", "foo"); }
+UTEST(c, ASSERT_STREQ) { ASSERT_STREQ("foo", "foo"); }
 
-TEST(c, ASSERT_STRNE) { ASSERT_STRNE("foo", "bar"); }
+UTEST(c, ASSERT_STRNE) { ASSERT_STRNE("foo", "bar"); }
 
-TEST(c, ASSERT_STRNEQ) { ASSERT_STRNEQ("foo", "foobar", strlen("foo")); }
+UTEST(c, ASSERT_STRNEQ) { ASSERT_STRNEQ("foo", "foobar", strlen("foo")); }
 
-TEST(c, ASSERT_STRNNE) { ASSERT_STRNNE("foo", "barfoo", strlen("foo")); }
+UTEST(c, ASSERT_STRNNE) { ASSERT_STRNNE("foo", "barfoo", strlen("foo")); }
 
-TEST(c, CHECK_TRUE) { CHECK_TRUE(1); }
+UTEST(c, EXPECT_TRUE) { EXPECT_TRUE(1); }
 
-TEST(c, CHECK_FALSE) { CHECK_FALSE(0); }
+UTEST(c, EXPECT_FALSE) { EXPECT_FALSE(0); }
 
-TEST(c, CHECK_EQ) { CHECK_EQ(1, 1); }
+UTEST(c, EXPECT_EQ) { EXPECT_EQ(1, 1); }
 
-TEST(c, CHECK_NE) { CHECK_NE(1, 2); }
+UTEST(c, EXPECT_NE) { EXPECT_NE(1, 2); }
 
-TEST(c, CHECK_LT) { CHECK_LT(1, 2); }
+UTEST(c, EXPECT_LT) { EXPECT_LT(1, 2); }
 
-TEST(c, CHECK_LE) {
-  CHECK_LE(1, 1);
-  CHECK_LE(1, 2);
+UTEST(c, EXPECT_LE) {
+  EXPECT_LE(1, 1);
+  EXPECT_LE(1, 2);
 }
 
-TEST(c, CHECK_GT) { CHECK_GT(2, 1); }
+UTEST(c, EXPECT_GT) { EXPECT_GT(2, 1); }
 
-TEST(c, CHECK_GE) {
-  CHECK_GE(1, 1);
-  CHECK_GE(2, 1);
+UTEST(c, EXPECT_GE) {
+  EXPECT_GE(1, 1);
+  EXPECT_GE(2, 1);
 }
 
-TEST(c, CHECK_STREQ) { CHECK_STREQ("foo", "foo"); }
+UTEST(c, EXPECT_STREQ) { EXPECT_STREQ("foo", "foo"); }
 
-TEST(c, CHECK_STRNE) { CHECK_STRNE("foo", "bar"); }
+UTEST(c, EXPECT_STRNE) { EXPECT_STRNE("foo", "bar"); }
 
-TEST(c, CHECK_STRNEQ) { CHECK_STRNEQ("foo", "foobar", strlen("foo")); }
+UTEST(c, EXPECT_STRNEQ) { EXPECT_STRNEQ("foo", "foobar", strlen("foo")); }
 
-TEST(c, CHECK_STRNNE) { CHECK_STRNNE("foo", "barfoo", strlen("foo")); }
+UTEST(c, EXPECT_STRNNE) { EXPECT_STRNNE("foo", "barfoo", strlen("foo")); }
 
-TEST(foo, bar) {
-    ASSERT_TRUE(1);
-    ASSERT_TRUE(0);
-    CHECK_TRUE(1 == 1);
-    CHECK_TRUE(2 != 3);
-    ASSERT_TRUE(1 == 1);
-    ASSERT_TRUE(2 != 3);
-}
+// UTEST(foo, bar) {
+//     ASSERT_TRUE(1);
+//     ASSERT_TRUE(0);
+//     CHECK_TRUE(1 == 1);
+//     CHECK_TRUE(2 != 3);
+//     ASSERT_TRUE(1 == 1);
+//     ASSERT_TRUE(2 != 3);
+// }
 
-TEST(foo, b) {
-    ASSERT_TRUE(1);
-    ASSERT_TRUE(1);
-    ASSERT_TRUE(1 == 1);
-    ASSERT_TRUE(2 != 3);
-}
+// UTEST(foo, b) {
+//     ASSERT_TRUE(1);
+//     ASSERT_TRUE(1);
+//     ASSERT_TRUE(1 == 1);
+//     ASSERT_TRUE(2 != 3);
+// }
  
+
 
 // int main(int argc, const char* const argv[]) {
 //     // std::string buffer = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
