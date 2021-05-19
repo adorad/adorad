36dccedddac7a0950932b33d078fc78f97b68c80
Jason Dsouza <jasmcaus@gmail.com>
2021-05-19 11:53:18 +0530

    Update Lexer tests

--- test/Compiler/test_Lexer.c
+++ test/Compiler/test_Lexer.c
@@ -2,27 +2,29 @@
 #include <Muon/Muon.h>
 MUON_MAIN()
 
+// These tests will only pass if the Lexer functions it tests aren't declared as static, inline or both
+
 TEST(Lexer, Init) {
     char* buffer = "0123456789abcdefghijklmnopqrstuvwxyz";
     Lexer* lexer = lexer_init(buffer);
 
-    CHECK(lexer->buffer__ != "", "Buffer must not be empty");
+    CHECK(strcmp(lexer->buffer__, "") != 0, "Buffer must not be empty");
     CHECK(lexer->buffer_capacity__ == 36, "Buffer capacity mismatch");
     CHECK(lexer->offset__ == 0, "Offset must be 0");
-    CHECK(lexer->location__.lineno__ == 1, "Lineno must be 0");
-    CHECK(lexer->location__.colno__ == 0, "Colno must be 0");
-    CHECK(lexer->location__.fname__ == "", "FName must be empty");
+    CHECK(lexer->location__.lineno__ == 1, "Lineno must be 1");
+    CHECK(lexer->location__.colno__ == 1, "Colno must be 1");
+    CHECK(strcmp(lexer->location__.fname__, "") == 0, "Filename must be empty");
 
     free(lexer);
-    free(buffer);
 }
 
 TEST(Lexer, next) {
     char* buffer = "0123456789abcdefghijklmnopqrstuvwxyz";
     Lexer* lexer = lexer_init(buffer);
+    
+    CHECK(strcmp(lexer->buffer__, buffer) == 0, "Buffer lengths are not the same");
     CHECK(lexer_next(lexer) == '0', "Needs to be `0`");
-    CHECK(strcmp(lexer->buffer__, buffer), "Buffer lengths are not the same");
     CHECK(lexer->offset__ == 1, "Offset needs to be 1");
-    CHECK(lexer->location__.colno__ == 1, "Colno needs to be 1");
+    CHECK(lexer->location__.colno__ == 2, "Colno needs to be 2");
     CHECK(lexer->location__.lineno__ == 1, "Lineno needs to be 1");
 }
