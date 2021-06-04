47a2532e1f13a93ed0e7c0c0dbd637eb20991258
Jason Dsouza <jasmcaus@gmail.com>
2021-06-04 11:45:46 +0530

    Rename

--- Hazel/hazel.h
+++ Hazel/hazel.h
@@ -19,5 +19,3 @@ Copyright (c) 2021 Jason Dsouza <http://github.com/jasmcaus>
 #include <Hazel/Compiler/Types/Types.h> 
 #include <Hazel/Compiler/Lexer/Lexer.h>
 #include <Hazel/Compiler/Tokens/Tokens.h> 
-// #include <Hazel/Compiler/Ast/Ast.h>
-// #include <Hazel/Compiler/Parser/Parser.h> 
