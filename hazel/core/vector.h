641c249e1b84d24c6d7263cea9b20460fd272e23
Jason Dsouza <jasmcaus@gmail.com>
2021-06-15 13:04:26 +0530

    Remove `main()` in `vector.h`

--- hazel/core/vector.h
+++ hazel/core/vector.h
@@ -258,28 +258,3 @@ static bool vec_grow(cstlVector* vec, UInt64 capacity) {
 
 	return true;
 }
-
-// int main(int argc, char *argv[])
-// {
-//     int i =0;
-//     //init vector
-//     VECTOR_INIT(v);
-//     //Add data in vector
-//     v.pfVectorAdd(&v,"aticleworld.com\n");
-//     v.pfVectorAdd(&v,"amlendra\n");
-//     v.pfVectorAdd(&v,"Pooja\n");
-//     //print the data and type cast it
-//     for (i = 0; i < v.pfVectorTotal(&v); i++)
-//     {
-//         printf("%s", (char*)v.pfVectorGet(&v, i));
-//     }
-//     //Set the data at index 0
-//     v.pfVectorSet(&v,0,"Apoorv\n");
-//     printf("\n\n\nVector list after changes\n\n\n");
-//     //print the data and type cast it
-//     for (i = 0; i < v.pfVectorTotal(&v); i++)
-//     {
-//         printf("%s", (char*)v.pfVectorGet(&v, i));
-//     }
-//     return 0;
-// }
