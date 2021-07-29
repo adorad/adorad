8f5ed178d613c3f3a2460947be02bd72bcc03bd4
Jason Dsouza <jasmcaus@gmail.com>
2021-07-29 11:46:19 +0530

    Update copyright

--- adorad/core/buffer.h
+++ adorad/core/buffer.h
@@ -1,10 +1,10 @@
 /*
-          _____         _____           _____  
-    /\   |  __ \ / __ \|  __ \    /\   |  __ \
-   /  \  | |  | | |  | | |__)|   /  \  | |  | | Adorad - The Fast, Expressive & Elegant Programming Language
-  / /\ \ | |  | | |  | |  _  /  / /\ \ | |  | | Languages: C, C++, and Assembly
- / ____ \| |__| | |__| | | \ \ / ____ \| |__| | https://github.com/adorad/adorad/
-/_/_ __\_\_____/_\____/|_|__\_/_/_ __\_\_____/
+          _____   ____  _____            _____  
+    /\   |  __ \ / __ \|  __ \     /\   |  __ \ 
+   /  \  | |  | | |  | | |__) |   /  \  | |  | | Adorad - The Fast, Expressive & Elegant Programming Language
+  / /\ \ | |  | | |  | |  _  /   / /\ \ | |  | | Languages: C, C++, and Assembly
+ / ____ \| |__| | |__| | | \ \  / ____ \| |__| | https://github.com/adorad/adorad/
+/_/    \_\_____/ \____/|_|  \_\/_/    \_\_____/ 
 
 Licensed under the MIT License <http://opensource.org/licenses/MIT>
 SPDX-License-Identifier: MIT
