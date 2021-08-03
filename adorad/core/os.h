fc1280353ae9604988bc96060d4e21d2e64ce8b2
Jason Dsouza <jasmcaus@gmail.com>
2021-08-03 15:23:56 +0530

    Add `os_path_extname`

--- adorad/core/os.h
+++ adorad/core/os.h
@@ -21,71 +21,6 @@ Copyright (c) 2021 Jason Dsouza <@jasmcaus>
 #include <adorad/core/types.h>
 #include <adorad/core/debug.h>
 
-#if defined(_WIN32) || defined(_WIN64)
-    #ifndef CSTL_OS_WINDOWS
-        #define CSTL_OS_WINDOWS 1
-    #endif
-#elif defined(__APPLE__) && defined(__MACH__)
-    #ifndef CSTL_OS_OSX
-        #define CSTL_OS_OSX 1
-    #endif
-#elif defined(unix) || defined(__unix__) || defined(__unix) || defined(__APPLE__)
-    #ifndef CSTL_OS_UNIX
-        #define CSTL_OS_UNIX 1
-    #endif
-
-    #if defined(__linux__)
-        #ifndef CSTL_OS_LINUX
-            #define CSTL_OS_LINUX 1
-        #endif
-    #elif defined(__FreeBSD__) && defined(__FreeBSD_kernel__)
-        #ifndef CSTL_OS_FREEBSD
-            #define CSTL_OS_FREEBSD 1
-        #endif
-    #else 
-        #error This Unix Operating System is not supported by Adorad.
-    #endif 
-#elif defined(_gnu_linux_) || defined(__linux__) && !defined(CSTL_OS_LINUX)
-    #define CSTL_OS_LINUX 1
-#elif defined(__NetBSD__)
-    #define CSTL_OS_NETBSD
-#elif defined(__DragonFly__)
-    #define CSTL_OS_DRAGONFLY
-#elif defined(__OpenBSD__)
-    #define CSTL_OS_OPENBSD
-#elif defined(__HAIKU__)
-    #define CSTL_OS_HAIKU
-#else
-    #error This Operating System is not supported by Adorad
-#endif
-
-// Architetures
-#if defined(__x86_64__)
-    #define CSTL_ARCH_X86_64    1
-#elif defined(__aarch64__)
-    #define CSTL_ARCH_ARM64     1
-#elif defined(__ATM_EABI__)
-    #define CSTL_ARCH_ARM       1
-#else
-    #define CSTL_ARCH_UNKNOWN
-    // #error This Architecture is not supported by Adorad
-#endif // __x86_64
-
-// Generic 32 vs 64 bit
-#if defined(_WIN64) || defined(__x86_64__) || defined(_M_X64) || defined(__64BIT__) || defined(__powerpc64__) || defined(__ppc64__)
-    #define CSTL_ARCH_64BIT     1
-#else 
-    #define CSTL_ARCH_32BIT     1
-#endif // _WIN64
-
-#ifdef CSTL_OS_WINDOWS
-    #define CSTL_OS_SEP         "\\"
-    #define CSTL_OS_SEP_CHAR    '\\'
-#else
-    #define CSTL_OS_SEP         "/"
-    #define CSTL_OS_SEP_CHAR    '/'
-#endif // CSTL_OS_WINDOWS
-
 static Buff* os_get_cwd();
 static Buff* os_path_dirname(Buff* path);
 static Buff* os_path_extname(Buff* path);
@@ -160,7 +95,18 @@ static Buff* os_get_basename(Buff* path) {
 }
 
 static Buff* os_path_extname(Buff* path) {
+    Buff* basename = os_get_basename(path);
+    if(!strcmp(basename->data, ""))
+        return basename;
+    
+    char* ext = strchr(basename->data, '.');
+    if(!ext) {
+       free(basename);
+       return buff_new(null);
+    }
 
+    buff_set(basename, ext);
+    return basename;
 }
 
 static Buff* os_path_join(Buff* path) {
