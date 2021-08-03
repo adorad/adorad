fa8317178b89ea6da98429f2de92447540b078f0
Jason Dsouza <jasmcaus@gmail.com>
2021-08-03 14:32:12 +0530

    Update

--- adorad/core/os.h
+++ adorad/core/os.h
@@ -144,7 +144,7 @@ static Buff* __os_dirname_basename(Buff* path, bool is_basename) {
         }
         buff_set(result, rev->data);
         result = buff_rev(result);
-        buff_free(rev);
+        free(rev);
     }
     
     return result;
