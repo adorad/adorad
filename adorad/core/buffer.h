78cdf8539b16a047cb11da99cdfed30e48f6aa3c
Jason Dsouza <jasmcaus@gmail.com>
2021-08-05 16:06:09 +0530

    Update buffer.h

--- adorad/core/buffer.h
+++ adorad/core/buffer.h
@@ -52,6 +52,7 @@ static cstlBuffer* buff_new(char* buff_data) {
     cstlBuffer* buffer = (cstlBuffer*)calloc(1, sizeof(cstlBuffer));
     CSTL_CHECK_NOT_NULL(buffer, "Could not allocate memory. Memory full.");
 
+    buffer->is_utf8 = false;
     buff_set(buffer, buff_data);
 
     return buffer;
@@ -96,69 +97,87 @@ static bool buff_is_empty(cstlBuffer* buffer) {
 // Note: The implementation for this was taken from:
 // https://github.com/lattera/glibc/blob/master/string/strlen.c
 // All due credit for this goes to the rightful author.
-static UInt32 __internal_strlength(const char* str) {
-    const char* char_ptr;
-    const unsigned long int* longword_ptr;
-    unsigned long int longword, himagic, lomagic;
+static UInt32 __internal_strlength(const char* str, bool is_utf8) {
+    if(!is_utf8) {
+        const char* char_ptr;
+        const unsigned long int* longword_ptr;
+        unsigned long int longword, himagic, lomagic;
 
-    // Handle the first few characters by reading one character at a time.
-    // Do this until CHAR_PTR is aligned on a longword boundary.
-    for (char_ptr = str; ((unsigned long int) char_ptr & (sizeof (longword) - 1)) != 0; ++char_ptr) {
-        if (*char_ptr == nullchar)
-            return char_ptr - str;
-    }
+        // Handle the first few characters by reading one character at a time.
+        // Do this until CHAR_PTR is aligned on a longword boundary.
+        for (char_ptr = str; ((unsigned long int) char_ptr & (sizeof (longword) - 1)) != 0; ++char_ptr) {
+            if (*char_ptr == nullchar)
+                return char_ptr - str;
+        }
 
-    // All these elucidatory comments refer to 4-byte longwords, but the theory applies equally 
-    // well to 8-byte longwords
-    longword_ptr = (unsigned long int* ) char_ptr;
+        // All these elucidatory comments refer to 4-byte longwords, but the theory applies equally 
+        // well to 8-byte longwords
+        longword_ptr = (unsigned long int* ) char_ptr;
 
-    // Bits 31, 24, 16, and 8 of this number are zero. Call these bits the "holes".
-    // Note that there is a hole just to the left of each byte, with an extra at the end:
-    //     bits:  01111110 11111110 11111110 11111111
-    //     bytes: AAAAAAAA BBBBBBBB CCCCCCCC DDDDDDDD
-    // The 1-bits make sure that carries propagate to the next 0-bit.
-    // The 0-bits provide holes for carries to fall into.
-    himagic = 0x80808080L;
-    lomagic = 0x01010101L;
+        // Bits 31, 24, 16, and 8 of this number are zero. Call these bits the "holes".
+        // Note that there is a hole just to the left of each byte, with an extra at the end:
+        //     bits:  01111110 11111110 11111110 11111111
+        //     bytes: AAAAAAAA BBBBBBBB CCCCCCCC DDDDDDDD
+        // The 1-bits make sure that carries propagate to the next 0-bit.
+        // The 0-bits provide holes for carries to fall into.
+        himagic = 0x80808080L;
+        lomagic = 0x01010101L;
 
-    if(sizeof(longword) > 4) {
-        // 64-bit version of the magic.
-        // Do the shift in two steps to avoid a warning if long has 32 bits.
-        himagic = ((himagic << 16) << 16) | himagic;
-        lomagic = ((lomagic << 16) << 16) | lomagic;
-    }
-    if(sizeof(longword) > 8)
-        exit(1);
+        if(sizeof(longword) > 4) {
+            // 64-bit version of the magic.
+            // Do the shift in two steps to avoid a warning if long has 32 bits.
+            himagic = ((himagic << 16) << 16) | himagic;
+            lomagic = ((lomagic << 16) << 16) | lomagic;
+        }
+        if(sizeof(longword) > 8)
+            exit(1);
 
-    // Instead of the traditional loop which tests each character, we will test a longword at a time.
-    // The tricky part is testing if *any of the four* bytes in the longword in question are zero.
-    for (;;) {
-        longword = *longword_ptr++;
+        // Instead of the traditional loop which tests each character, we will test a longword at a time.
+        // The tricky part is testing if *any of the four* bytes in the longword in question are zero.
+        for (;;) {
+            longword = *longword_ptr++;
 
-        if (((longword - lomagic) & ~longword & himagic) != 0) {
-            // Which of the bytes was the zero?  If none of them were, it was a misfire; continue the search.
-            const char* cp = (const char* ) (longword_ptr - 1);
+            if (((longword - lomagic) & ~longword & himagic) != 0) {
+                // Which of the bytes was the zero?  If none of them were, it was a misfire; continue the search.
+                const char* cp = (const char* ) (longword_ptr - 1);
 
-            if (cp[0] == 0)
-                return cp - str;
-            if (cp[1] == 0)
-                return cp - str + 1;
-            if (cp[2] == 0)
-                return cp - str + 2;
-            if (cp[3] == 0)
-                return cp - str + 3;
+                if (cp[0] == 0)
+                    return cp - str;
+                if (cp[1] == 0)
+                    return cp - str + 1;
+                if (cp[2] == 0)
+                    return cp - str + 2;
+                if (cp[3] == 0)
+                    return cp - str + 3;
 
-            if (sizeof (longword) > 4) {
-                if (cp[4] == 0)
-                    return cp - str + 4;
-                if (cp[5] == 0)
-                    return cp - str + 5;
-                if (cp[6] == 0)
-                    return cp - str + 6;
-                if (cp[7] == 0)
-                    return cp - str + 7;
+                if (sizeof (longword) > 4) {
+                    if (cp[4] == 0)
+                        return cp - str + 4;
+                    if (cp[5] == 0)
+                        return cp - str + 5;
+                    if (cp[6] == 0)
+                        return cp - str + 6;
+                    if (cp[7] == 0)
+                        return cp - str + 7;
+                }
             }
         }
+    } 
+    // UTF-8
+    else {
+        UInt32 count = 0;
+        for(; *str; count++) {
+            char c = *str;
+            UInt32 increment = 0;
+                 if(c < 0x80)               increment = 1;
+            else if((c & 0xe0) == 0xc0)     increment = 2;
+            else if((c & 0xf0) == 0xe0)     increment = 3;
+            else if((c & 0xf8) == 0xf0)     increment = 4;
+            else return -1;
+            
+            str += increment;
+        }
+        return count;
     }
 }
 
@@ -200,7 +219,7 @@ static void buff_set(cstlBuffer* buffer, char* new_buff) {
         len = 0;
         new_buff = "";
     } else {
-        len = (UInt64)__internal_strlength(new_buff);
+        len = (UInt64)__internal_strlength(new_buff, buffer->is_utf8);
     }
 
     buffer->data = new_buff;
