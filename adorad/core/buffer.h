a99b8110abdfc401d99e723199fbb20834302e4d
Jason Dsouza <jasmcaus@gmail.com>
2021-08-05 12:33:06 +0530

    Add `buff_len` and `__internal_strlength`

--- adorad/core/buffer.h
+++ adorad/core/buffer.h
@@ -24,36 +24,23 @@ typedef cstlBuffer Buff;
 
 struct cstlBuffer {
     char* data;    // buffer data
-    UInt64 length; // buffer size
+    UInt64 len;    // buffer size
 };
 
-// Create a new `cstlBuffer`
+/
 static cstlBuffer* buff_new(char* buff_data);
-// Return the n'th character in the buffer data
 static char buff_at(cstlBuffer* buffer, UInt64 n);
-// Return a pointer to the beginning of the buffer data
 static char* buff_begin(cstlBuffer* buffer);
-// Return a pointer to the end of the buffer data
 static char* buff_end(cstlBuffer* buffer);
-// Is the buffer data empty?
 static bool buff_is_empty(cstlBuffer* buffer);
-// Append `buff2` to the buffer data
 static void buff_append(cstlBuffer* buffer, cstlBuffer* buff2);
-// Assign `new_buff` to the buffer data
 static void buff_set(cstlBuffer* buffer, char* new_buff);
-// Reset a buffer
+static UInt32 buff_len(cstlBuffer* buffer);
 static void buff_reset(cstlBuffer* buffer);
-// Reverse a buffer (non-destructive)
 static cstlBuffer* buff_rev(cstlBuffer* buffer);
-// Compare two buffers (case-sensitive)
-// Returns true if `buff1` is lexicographically equal to `buff2`
 static bool buff_cmp(cstlBuffer* buff1, cstlBuffer* buff2);
-// Compare two buffers (ignoring case)
-// Returns true if `buff1` is lexicographically equal to `buff2`
 static bool buff_cmp_nocase(cstlBuffer* buff1, cstlBuffer* buff2);
-// Get a slice of a buffer
 static cstlBuffer* buff_slice(cstlBuffer* buffer, int begin, int bytes);
-// Free the buffer from its associated memory
 static void buff_free(cstlBuffer* buffer);
 
 // Create a new `cstlBuffer`
@@ -71,13 +58,13 @@ static char buff_at(cstlBuffer* buffer, UInt64 n) {
     CSTL_CHECK_NOT_NULL(buffer, "Expected not null");
     CSTL_CHECK_NOT_NULL(buffer->data, "Expected not null");
     
-    if(n >= buffer->length)
+    if(n >= buffer->len)
         return nullchar;
 
     return (char)buffer->data[n];
 }
 
-// Return a pointer to the beginning of the buffer data
+// Returns a pointer to the beginning of the buffer data
 static char* buff_begin(cstlBuffer* buffer) {
     CSTL_CHECK_NOT_NULL(buffer, "Expected not null");
     CSTL_CHECK_NOT_NULL(buffer->data, "Expected not null");
@@ -85,12 +72,12 @@ static char* buff_begin(cstlBuffer* buffer) {
     return (char*)buffer->data;
 }
 
-// Return a pointer to the end of the buffer data
+// Returns a pointer to the end of the buffer data
 static char* buff_end(cstlBuffer* buffer) {
     CSTL_CHECK_NOT_NULL(buffer, "Expected not null");
     CSTL_CHECK_NOT_NULL(buffer->data, "Expected not null");
 
-    return (char*)(buffer->data + ((buffer->length - 1) * sizeof(char)));
+    return (char*)(buffer->data + ((buffer->len - 1) * sizeof(char)));
 }
 
 // Is the buffer data empty?
@@ -98,7 +85,77 @@ static bool buff_is_empty(cstlBuffer* buffer) {
     CSTL_CHECK_NOT_NULL(buffer, "Expected not null");
     CSTL_CHECK_NOT_NULL(buffer->data, "Expected not null");
 
-    return buffer->length == 0;
+    return buffer->len == 0;
+}
+
+// Returns the length of the buffer
+// Note: The implementation for this was taken from:
+// https://github.com/lattera/glibc/blob/master/string/strlen.c
+// All due credit for this goes to the rightful author.
+static UInt32 __internal_strlength(const char* str) {
+    const char* char_ptr;
+    const unsigned long int* longword_ptr;
+    unsigned long int longword, himagic, lomagic;
+
+    // Handle the first few characters by reading one character at a time.
+    // Do this until CHAR_PTR is aligned on a longword boundary.
+    for (char_ptr = str; ((unsigned long int) char_ptr & (sizeof (longword) - 1)) != 0; ++char_ptr) {
+        if (*char_ptr == nullchar)
+            return char_ptr - str;
+    }
+
+    // All these elucidatory comments refer to 4-byte longwords, but the theory applies equally 
+    // well to 8-byte longwords
+    longword_ptr = (unsigned long int* ) char_ptr;
+
+    // Bits 31, 24, 16, and 8 of this number are zero. Call these bits the "holes".
+    // Note that there is a hole just to the left of each byte, with an extra at the end:
+    //     bits:  01111110 11111110 11111110 11111111
+    //     bytes: AAAAAAAA BBBBBBBB CCCCCCCC DDDDDDDD
+    // The 1-bits make sure that carries propagate to the next 0-bit.
+    // The 0-bits provide holes for carries to fall into.
+    himagic = 0x80808080L;
+    lomagic = 0x01010101L;
+
+    if(sizeof(longword) > 4) {
+        // 64-bit version of the magic.
+        // Do the shift in two steps to avoid a warning if long has 32 bits.
+        himagic = ((himagic << 16) << 16) | himagic;
+        lomagic = ((lomagic << 16) << 16) | lomagic;
+    }
+    if(sizeof(longword) > 8)
+        exit(1);
+
+    // Instead of the traditional loop which tests each character, we will test a longword at a time.
+    // The tricky part is testing if *any of the four* bytes in the longword in question are zero.
+    for (;;) {
+        longword = *longword_ptr++;
+
+        if (((longword - lomagic) & ~longword & himagic) != 0) {
+            // Which of the bytes was the zero?  If none of them were, it was a misfire; continue the search.
+            const char* cp = (const char* ) (longword_ptr - 1);
+
+            if (cp[0] == 0)
+                return cp - str;
+            if (cp[1] == 0)
+                return cp - str + 1;
+            if (cp[2] == 0)
+                return cp - str + 2;
+            if (cp[3] == 0)
+                return cp - str + 3;
+
+            if (sizeof (longword) > 4) {
+                if (cp[4] == 0)
+                    return cp - str + 4;
+                if (cp[5] == 0)
+                    return cp - str + 5;
+                if (cp[6] == 0)
+                    return cp - str + 6;
+                if (cp[7] == 0)
+                    return cp - str + 7;
+            }
+        }
+    }
 }
 
 // Append `buff2` to the buffer data
@@ -109,7 +166,7 @@ static void buff_append(cstlBuffer* buffer, cstlBuffer* buff2) {
     CSTL_CHECK_NOT_NULL(buff2, "Expected not null");
     CSTL_CHECK_NOT_NULL(buff2->data, "Expected not null");
 
-    UInt64 new_len = buffer->length + buff2->length + 1;
+    UInt64 new_len = buffer->len + buff2->len + 1;
     char* newstr = (char*)calloc(1, new_len);
     strcpy(newstr, buffer->data);
     strcat(newstr, buff2->data);
@@ -121,13 +178,13 @@ static void buff_append_char(cstlBuffer* buffer, char ch) {
     CSTL_CHECK_NOT_NULL(buffer, "Expected not null");
     CSTL_CHECK_NOT_NULL(buffer->data, "Expected not null");
 
-    UInt64 len = buffer->length;
+    UInt64 len = buffer->len;
     char* newstr = (char*)calloc(1, len + 1);
     strcpy(newstr, buffer->data);
     newstr[len] = ch;
     newstr[len + 1] = nullchar;
     buff_set(buffer, newstr);
-    buffer->length += 2;
+    buffer->len += 2;
 }
 
 // Assign `new_buff` to the buffer data
@@ -139,11 +196,16 @@ static void buff_set(cstlBuffer* buffer, char* new_buff) {
         len = 0;
         new_buff = "";
     } else {
-        len = (UInt64)strlen(new_buff);
+        len = (UInt64)__internal_strlength(new_buff);
     }
 
     buffer->data = new_buff;
-    buffer->length = len;
+    buffer->len = len;
+}
+
+// Returns the buffer length
+static UInt32 buff_len(cstlBuffer* buffer) {
+    return buffer->len;
 }
 
 // Free the buffer from its associated memory
@@ -152,13 +214,13 @@ static void buff_reset(cstlBuffer* buffer) {
         return;
 
     buffer->data = null;
-    buffer->length = 0;
+    buffer->len = 0;
 }
 
 // Reverse a buffer (non-destructive)
 static Buff* buff_rev(Buff* buffer) {
     Buff* rev = buff_new(null);
-    UInt64 length = buffer->length;
+    UInt64 length = buffer->len;
     if(!length)
         return rev;
     
@@ -173,7 +235,7 @@ static Buff* buff_rev(Buff* buffer) {
 // Compare two buffers (case-sensitive)
 // Returns true if `buff1` is lexicographically equal to `buff2`
 static bool buff_cmp(cstlBuffer* buff1, cstlBuffer* buff2) {
-    if(buff1->length != buff2->length)
+    if(buff1->len != buff2->len)
         return false;
     
     const unsigned char* s1 = (const unsigned char*) buff1->data;
@@ -193,7 +255,7 @@ static bool buff_cmp(cstlBuffer* buff1, cstlBuffer* buff2) {
 // Compare two buffers (ignoring case)
 // Returns true if `buff1` is lexicographically equal to `buff2`
 static bool buff_cmp_nocase(cstlBuffer* buff1, cstlBuffer* buff2) {
-    if(buff1->length != buff2->length)
+    if(buff1->len != buff2->len)
         return false;
     
     const unsigned char* s1 = (const unsigned char*) buff1->data;
@@ -226,7 +288,8 @@ static cstlBuffer* buff_slice(cstlBuffer* buffer, int begin, int bytes) {
 
 // Free the buffer from its associated memory
 static void buff_free(cstlBuffer* buffer) {
-    free(buffer);
+    if(buffer)
+        free(buffer);
 }
 
 #endif // CSTL_BUFFER_H
