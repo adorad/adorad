2668eca63534029e1059621797a3b1ed257ddada
Jason Dsouza <jasmcaus@gmail.com>
2021-08-05 16:45:39 +0530

    Fix bug: rename `os_get_dirname` and `os_get_basename` to `os_path_dirname` and `os_path_basename` respectively

--- adorad/core/os.h
+++ adorad/core/os.h
@@ -29,10 +29,10 @@ Copyright (c) 2021 Jason Dsouza <@jasmcaus>
     #include <direct.h>
 #endif // CSTL_OS_WINDOWS
 
-static Buff* os_get_cwd();
-static Buff* os_path_dirname(Buff* path);
-static Buff* os_path_extname(Buff* path);
-static Buff* os_path_join(Buff* path1, Buff* path2);
+static cstlBuffer* os_get_cwd();
+static cstlBuffer* os_path_dirname(cstlBuffer* path);
+static cstlBuffer* os_path_extname(cstlBuffer* path);
+static cstlBuffer* os_path_join(cstlBuffer* path1, cstlBuffer* path2);
 static bool os_is_sep(char ch);
 
 #ifndef CSTL_OS_FUNC_ALIASES
@@ -42,7 +42,7 @@ static bool os_is_sep(char ch);
     #define ospj    os_path_join
 #endif // CSTL_OS_FUNC_ALIASES
 
-static Buff* os_get_cwd() {
+static cstlBuffer* os_get_cwd() {
 #if defined(CSTL_OS_WINDOWS)
     // This (or its equivalent) is not defined in any include in Windows as far as I've come across
     #define PATH_MAX 4096
@@ -52,7 +52,7 @@ static Buff* os_get_cwd() {
         fprintf(stderr, "Unable to `os_get_cwd()`. 'getcwd()' failed");
         exit(1);
     }
-    Buff* buff = buff_new(result);
+    cstlBuffer* buff = buff_new(result);
     return buff;
 #elif defined(CSTL_OS_POSIX)
     long n;
@@ -67,7 +67,7 @@ static Buff* os_get_cwd() {
         fprintf(stderr, "Unable to `os_get_cwd()`. 'getcwd()' failed");
         exit(1);
     }
-    Buff* buff = buff_new(buf);
+    cstlBuffer* buff = buff_new(buf);
     return buff;
 #else
     #error "No `os_get_cwd()` implementation supported for your platform."
@@ -75,12 +75,12 @@ static Buff* os_get_cwd() {
 #endif // CSTL_OS_WINDOWS
 }
 
-static Buff* __os_dirname_basename(Buff* path, bool is_basename) {
+static cstlBuffer* __os_dirname_basename(cstlBuffer* path, bool is_basename) {
     UInt64 length = path->len;
     if(!length)
         return path;
 
-    Buff* result = buff_new(null);
+    cstlBuffer* result = buff_new(null);
     char* end = buff_end(path);
     if(!is_basename) {
         // If the last character is a `sep`, `path` is the dirname
@@ -91,7 +91,7 @@ static Buff* __os_dirname_basename(Buff* path, bool is_basename) {
         if(!(strstr(path->data, "/") || strstr(path->data, "\\")))
             return result;
         
-        Buff* rev = buff_rev(path);
+        cstlBuffer* rev = buff_rev(path);
         char* rev_dir = strchr(rev->data, *(CSTL_OS_SEP));
         buff_set(result, rev_dir);
         result = buff_rev(result);
@@ -105,7 +105,7 @@ static Buff* __os_dirname_basename(Buff* path, bool is_basename) {
         if(!(strstr(path->data, "/") || strstr(path->data, "\\")))
             return path;
         
-        Buff* rev = buff_rev(path);
+        cstlBuffer* rev = buff_rev(path);
         for(UInt64 i = 0; i<length; i++) {
             if(os_is_sep(*(rev->data + i))) {
                 *(rev->data + i) = nullchar;
@@ -120,16 +120,16 @@ static Buff* __os_dirname_basename(Buff* path, bool is_basename) {
     return result;
 }
 
-static Buff* os_get_dirname(Buff* path) {
+static cstlBuffer* os_path_dirname(cstlBuffer* path) {
     return __os_dirname_basename(path, false);
 }
 
-static Buff* os_get_basename(Buff* path) {
+static cstlBuffer* os_path_basename(cstlBuffer* path) {
     return __os_dirname_basename(path, true);
 }
 
-static Buff* os_path_extname(Buff* path) {
-    Buff* basename = os_get_basename(path);
+static cstlBuffer* os_path_extname(cstlBuffer* path) {
+    cstlBuffer* basename = os_path_basename(path);
     if(!strcmp(basename->data, ""))
         return basename;
     
@@ -144,7 +144,7 @@ static Buff* os_path_extname(Buff* path) {
 }
 
 
-static Buff* os_path_join(Buff* path1, Buff* path2) {
+static cstlBuffer* os_path_join(cstlBuffer* path1, cstlBuffer* path2) {
     UInt64 length = path1->len;
     if(!length)
         return path1;
