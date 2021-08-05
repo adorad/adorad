7f9bc2bee5ee678e1ce5d1e6fa369c6daba75143
Jason Dsouza <jasmcaus@gmail.com>
2021-08-05 14:23:30 +0530

    Update internal tests

--- adorad/maininternaltests.c
+++ adorad/maininternaltests.c
@@ -3,6 +3,7 @@
 int main(int argc, const char* const argv[]) {
     // Buff* buf = os_get_cwd();
     // printf("CWD: %s\n", buf->data);
+
 	char* buffer = readFile("/home/jas/Documents/dev/adorad/test/LexerDemo.ad");
     // char* buffer = "0123456789abcdefghijklmnopqrstuvwxyz";
 	Lexer* lexer = lexer_init(buffer, "test/LexerDemo.ad"); 
@@ -18,12 +19,11 @@ int main(int argc, const char* const argv[]) {
     printf("Lexing finished...\n");
     double total = duration(st, end);
 
-    // printf("\033[1;32m\nTokens Vector: \033[0m\n");
-    // for(UInt64 i=0; i < vec_size(lexer->toklist); i++) {
-    //     Token* tok = vec_at(lexer->toklist, i);
-    //     printf("TOKEN(%s, \"%s\")\n", token_to_buff(tok->kind), tok->value);
-    // } 
-    // printf("\nTotal time = %lfs\n", total);
+    printf("\033[1;32m\nTokens Vector: \033[0m\n");
+    for(Token* tok = vec_begin(lexer->toklist); tok <= vec_end(lexer->toklist); tok++) {
+        printf("TOKEN(%s, \"%s\")\n", token_to_buff(tok->kind)->data, tok->value->data);
+    }
+    printf("\nTotal time = %lfs\n", total);
 
     printf("Number of tokens = %d\n", vec_size(lexer->toklist));
     printf("Total allocated memory (in bytes) = %d\n", lexer->toklist->internal.objsize * vec_size(lexer->toklist));
