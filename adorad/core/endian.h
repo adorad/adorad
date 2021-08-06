e95cdf86d2e4413c3b894923127eb497b7e377fd
Jason Dsouza <jasmcaus@gmail.com>
2021-08-06 14:18:43 +0530

    Add endian swaps

--- adorad/core/endian.h
+++ adorad/core/endian.h
@@ -14,6 +14,8 @@ Copyright (c) 2021 Jason Dsouza <@jasmcaus>
 #ifndef CSTL_ENDIAN_H_
 #define CSTL_ENDIAN_H_
 
+#include <adorad/core/types.h>
+
 #if defined(__APPLE__)
     #include <machine/endian.h>
     #define CSTL_BIG_ENDIAN    BIG_ENDIAN
@@ -42,5 +44,25 @@ Copyright (c) 2021 Jason Dsouza <@jasmcaus>
 #else
     #error Unsupported endianness
 #endif
+
+static inline UInt16 endian_swap16(UInt16 i);
+static inline UInt32 endian_swap32(UInt32 i);
+static inline UInt64 endian_swap64(UInt64 i);
+
+static inline UInt16 endian_swap16(UInt16 i) {
+    return (i >> 8) | (i << 8);
+}
+
+static inline UInt32 endian_swap32(UInt32 i) {
+    return (i >> 24) | (i << 24) | ((i & 0x00ff0000u) >> 8) | ((i & 0x0000ff00u) << 8);
+}
+
+static inline UInt64 endian_swap64(UInt64 i) {
+    return (i >> 56) | (i << 56) |
+           ((i & 0x00ff000000000000ull) >> 40) | ((i & 0x000000000000ff00ull) << 40) |
+           ((i & 0x0000ff0000000000ull) >> 24) | ((i & 0x0000000000ff0000ull) << 24) |
+           ((i & 0x000000ff00000000ull) >> 8)  | ((i & 0x00000000ff000000ull) << 8);
+}
+
 
 #endif // CSTL_ENDIAN_H_
