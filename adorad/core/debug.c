116be4d1edfd7ccf52f64e468d0f44cef06deb9f
Jason Dsouza <jasmcaus@gmail.com>
2021-08-09 01:27:09 +0530

    Fix typo

--- adorad/core/debug.c
+++ adorad/core/debug.c
@@ -16,7 +16,7 @@ Copyright (c) 2021 Jason Dsouza <@jasmcaus>
 
 #if defined(_WIN32) || defined(__WIN32__) || defined(__WINDOWS__)
     #pragma warning(push, 0)
-        #include <Windows.h>
+        #include <windows.h>
         #include <io.h>
     #pragma warning(pop)
 #endif // _WIN32
