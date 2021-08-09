d3b98d1063172ad1489a0c2d85c631f3a67d12d6
Jason Dsouza <jasmcaus@gmail.com>
2021-08-09 10:46:21 +0530

    Update `utils.h`

--- adorad/core/utils.h
+++ adorad/core/utils.h
@@ -16,6 +16,31 @@ Copyright (c) 2021 Jason Dsouza <@jasmcaus>
 
 #include <adorad/core/compilers.h>
 
+#ifdef CSTL_COMPILER_MSVC
+    #define ATTRIBUTE_COLD        __declspec(noinline)
+    #define ATTRIBUTE_PRINTF(a,b)
+    #define ATTRIBUTE_NORETURN    __declspec(noreturn
+    #define ATTRIBUTE_WEAK
+    #define ATTRIBUTE_UNUSED
+    #define BREAKPOINT            __debugbreak())
+#else
+    #define ATTRIBUTE_COLD        __attribute((cold))
+    #define ATTRIBUTE_PRINTF(a,b) __attribute((format(printf, a, b)))
+    #define ATTRIBUTE_NORETURN    __attribute((noreturn))
+    #define ATTRIBUTE_WEAK        __attribute__((weak))
+    #define ATTRIBUTE_UNUSED      __attribute__((unused))
 
-
+    #if defined(__MINGW32__) || defined(__MINGW64__)
+        #define BREAKPOINT        __debugbreak()
+    #elif defined(__i386__) || defined(__x86_64__)
+        #define BREAKPOINT        __asm__ volatile("int $0x03");
+    #elif defined(__clang__)
+        #define BREAKPOINT        __builtin_debugtrap()
+    #elif defined(__GNUC__)
+        #define BREAKPOINT        __builtin_trap()
+    #else
+        #include <signal.h>
+        #define BREAKPOINT        raise(SIGTRAP)
+    #endif // __MINGW32__
+#endif // CSTL_COMPILER_MSVC
 #endif // CSTL_UTILS_H
