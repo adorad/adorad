d4def63e360a1a14adbbfb90a2ddce7335873a61
Jason Dsouza <jasmcaus@gmail.com>
2021-09-11 11:28:53 +0530

    Add compiler-specific `unreachable`

--- adorad/compiler/error.h
+++ adorad/compiler/error.h
@@ -41,11 +41,32 @@ ATTRIBUTE_NORETURN
 ATTRIBUTE_PRINTF(2, 3)
 void panic(Error err, const char* format, ...);
 
-#define unreachable()                                                                           \
-    panic(                                                                                      \
+#define unreachable()                                                                                  \
+    panic(                                                                                             \
         ErrorUnreachable,                                                                              \
         "Unreachable: At %s:%d in %s. %s", __FILE__, __LINE__, __func__,                               \
         "This is a bug in Adorad's compiler. Please file an issue on Adorad's Github repository"       \
     )
+
+// For a more compiler-specific `unreachable`, uncomment the following:
+/*
+#if !defined(unreachable)
+    #if defined(__GNUC__) && !defined(__clang__)
+        #define __GCC_VERSION  (__GNUC__ * 10000L + __GNUC_MINOR__ * 100L + __GNUC_PATCHLEVEL__)
+        #if (__GCC_VERSION >= 40500L)
+            #define unreachable()  do { __builtin_unreachable(); } while (0)
+        #endif
+        #undef __GCC_VERSION
+
+    #elif defined(__clang__) && defined(__has_builtin)
+        #if __has_builtin(__builtin_unreachable)
+            #define unreachable()  do { __builtin_unreachable(); } while (0)
+        #endif
+    
+    #else
+        #define unreachable()   do {} while(0);
+    #endif
+#endif // unreachable
+*/
 
 #endif // ADORAD_ERROR_H
