780aa0eaa299931d3498fc13e3814263c998789d
Jason Dsouza <jasmcaus@gmail.com>
2021-09-22 18:07:35 +0530

    Replace pattern `parse_*` with `ast_parse_*`

--- adorad/compiler/parser.c
+++ adorad/compiler/parser.c
@@ -1230,7 +1230,7 @@ static AstNode* ast_parse_field_init(Parser* parser) {
 // SuffixOp
 //      | LBRACKET Expr (DOT2 (Expr (COLON Expr)?)?)? RBRACKET
 //      | DOT IDENTIFIER
-static AstNode* parse_suffix_op(Parser* parser) {
+static AstNode* ast_parse_suffix_op(Parser* parser) {
     Token* lbrace = parser_chomp_if(LBRACE);
     if(lbrace != null) {
         AstNode* lower = parse_expr(parser);
