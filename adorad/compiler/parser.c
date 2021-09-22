474c5746a672aa1f7a4a66025ce6e93ad4b765cc
Jason Dsouza <jasmcaus@gmail.com>
2021-09-22 15:04:35 +0530

    Replace pattern `ast_parse_*` with `parse_*`

--- adorad/compiler/parser.c
+++ adorad/compiler/parser.c
@@ -1121,7 +1121,7 @@ static AstNode* parse_match_expr(Parser* parser) {
 
 // MatchBranch
 //      KEYWORD(case) (COLON? / EQUALS_ARROW?) AssignmentExpr
-static AstNode* ast_parse_match_branch(Parser* parser) {
+static AstNode* parse_match_branch(Parser* parser) {
     AstNode* out = ast_parse_match_case_kwd(parser);
     CORETEN_ENFORCE(out->kind == AstNodeKindMatchBranch);
     if(out == null)
