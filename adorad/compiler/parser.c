cc3993bcb568319c928883a21ecad1c15c5b2c08
Jason Dsouza <jasmcaus@gmail.com>
2021-09-22 15:03:47 +0530

    Replace pattern `ast_parse_*` with `parse_*`

--- adorad/compiler/parser.c
+++ adorad/compiler/parser.c
@@ -995,7 +995,7 @@ static AstNode* parse_suffix_expr(Parser* parser) {
 // and LabeledTypeExpr is one of:
 //      | BlockLabel Block
 //      | BlockLabel? LoopTypeExpr
-static AstNode* ast_parse_primary_type_expr(Parser* parser) {
+static AstNode* parse_primary_type_expr(Parser* parser) {
     AstNode* out = null;
     AstNode* expr = null;
     Token* tok = null;
