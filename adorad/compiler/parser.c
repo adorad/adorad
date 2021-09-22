7d5d936230d26bdef18c60b44a8f9a369dc162cf
Jason Dsouza <jasmcaus@gmail.com>
2021-09-22 18:04:07 +0530

    Replace pattern `parse_*` with `ast_parse_*`

--- adorad/compiler/parser.c
+++ adorad/compiler/parser.c
@@ -409,7 +409,7 @@ static AstNode* ast_parse_container_members(pars) {
 //      | IfPrefix AssignmentExpr (SEMICOLON / KEYWORD(else) Statement)?
 // where IfPrefix is:
 //      KEYWORD(if) LPAREN? Expr RPAREN?
-static AstNode* parse_if_expr(Parser* parser) {
+static AstNode* ast_parse_if_expr(Parser* parser) {
     Token* if_token = parser_chomp_if(IF);
     if(if_token == null)
         unreachable();
