f5f8c8e89594ad74940465cfc967fef024139788
Jason Dsouza <jasmcaus@gmail.com>
2021-09-22 18:06:55 +0530

    Replace pattern `parse_*` with `ast_parse_*`

--- adorad/compiler/parser.c
+++ adorad/compiler/parser.c
@@ -1100,7 +1100,7 @@ static AstNode* ast_parse_match_expr(Parser* parser) {
         ast_expected("`switch` keyword");
     
     Token* lparen = parser_chomp_if(LPAREN); // this is optional
-    AstNode* expr = parse_expr(parser);
+    AstNode* expr = ast_parse_expr(parser);
     if(expr == null)
         ast_expected("expression");
     Token* rparen = parser_chomp_if(RPAREN); // this is optional
