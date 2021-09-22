b50ccc3d4d176858b62f99133386eff970bcadca
Jason Dsouza <jasmcaus@gmail.com>
2021-09-22 15:00:22 +0530

    Replace pattern `ast_parse_*` with `parse_*`

--- adorad/compiler/parser.c
+++ adorad/compiler/parser.c
@@ -94,7 +94,7 @@ static AstNode* parse_toplevel_decl(Parser* parser) {
     if(import != null)
         return null;
     
-    AstNode* alias = ast_parse_alias_decl(parser);
+    AstNode* alias = parse_alias_decl(parser);
     if(alias != null)
         return alias;
     
