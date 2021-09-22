68181a388c398852e362d101c7fc91773cfcfc16
Jason Dsouza <jasmcaus@gmail.com>
2021-09-22 15:04:13 +0530

    Replace pattern `ast_parse_*` with `parse_*`

--- adorad/compiler/parser.c
+++ adorad/compiler/parser.c
@@ -1048,7 +1048,7 @@ static AstNode* parse_primary_type_expr(Parser* parser) {
                             }
                         case LOOP:
                             parser_chomp(2);
-                            return ast_parse_loop_expr(parser);
+                            return parse_loop_expr(parser);
                         case LBRACE:
                             parser_chomp(2);
                             return ast_parse_block(parser);
