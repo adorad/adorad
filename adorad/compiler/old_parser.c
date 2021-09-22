cd719b2759fd41d021c598c1d473e84a3baa5c10
Jason Dsouza <jasmcaus@gmail.com>
2021-09-22 17:57:46 +0530

    Replace pattern `parse_*` with `ast_parse_*`

--- adorad/compiler/old_parser.c
+++ adorad/compiler/old_parser.c
@@ -296,7 +296,7 @@ static AstNode* ast_parse_if_prefix(Parser* parser) {
 }
 
 static AstNode* ast_parse_if_expr(Parser* parser) {
-    AstNode* out = parse_if_prefix(parser);
+    AstNode* out = ast_parse_if_prefix(parser);
     if(out == null) {
         return null;
     }
