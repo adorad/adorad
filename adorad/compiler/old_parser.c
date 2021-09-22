86a20ed6c1bc60686f797b1d0dfb3386912208e1
Jason Dsouza <jasmcaus@gmail.com>
2021-09-22 17:57:08 +0530

    Replace pattern `parse_*` with `ast_parse_*`

--- adorad/compiler/old_parser.c
+++ adorad/compiler/old_parser.c
@@ -178,7 +178,7 @@ static AstNode* ast_parse_func_prototype(Parser* parser) {
     
     Token* identifier = parser_chomp_if(IDENTIFIER);
     Token* lparen = parser_expect_token(LPAREN);
-    Vec* params = ast_parse_param_list(parser, parse_match_branch);
+    Vec* params = ast_parse_param_list(parser, ast_parse_match_branch);
     Token* rparen = parser_expect_token(RPAREN);
 
     AstNode* return_type = parse_type_expr(parser);
