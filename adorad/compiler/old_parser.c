00ce46e7c5e026eda56e687e5f7f34d98ce2d68f
Jason Dsouza <jasmcaus@gmail.com>
2021-09-22 17:59:27 +0530

    Replace pattern `parse_*` with `ast_parse_*`

--- adorad/compiler/old_parser.c
+++ adorad/compiler/old_parser.c
@@ -606,7 +606,7 @@ static AstNode* ast_parse_boolean_and_expr(Parser* parser) {
 }
 
 // BooleanOrExpr
-static AstNode* parse_boolean_or_expr(Parser* parser) {
+static AstNode* ast_parse_boolean_or_expr(Parser* parser) {
     return parse_binary_op_expr(
         parser,
         BinaryOpChainInfinity,
