9d90c585ac08d639c481eff0e2718118db55c977
Jason Dsouza <jasmcaus@gmail.com>
2021-09-22 18:01:39 +0530

    Replace pattern `parse_*` with `ast_parse_*`

--- adorad/compiler/old_parser.c
+++ adorad/compiler/old_parser.c
@@ -784,7 +784,7 @@ static AstNode* ast_parse_init_list(Parser* parser) {
     out->data.expr->init_expr->kind = InitExprKindArray;
     out->data.expr->init_expr->entries = vec_new(AstNode, 1);
 
-    AstNode* first = parse_expr(parser);
+    AstNode* first = ast_parse_expr(parser);
     if(first != null) {
         vec_push(out->data.expr->init_expr->entries, first);
 
