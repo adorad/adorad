40a37112e15b3fa39b71066d8b2fad2b8869f871
Jason Dsouza <jasmcaus@gmail.com>
2021-09-22 15:05:29 +0530

    Replace pattern `ast_parse_*` with `parse_*`

--- adorad/compiler/parser.c
+++ adorad/compiler/parser.c
@@ -271,7 +271,7 @@ func_no_attrs:
             no_body = true;
             break;
         case LBRACE:
-            body = ast_parse_block(parser);
+            body = parse_block(parser);
             if(body == null)
                 ast_expected("Expected a body");
             break;
