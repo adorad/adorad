559e73aba72cab3673831f9e16dc75558306efdf
Jason Dsouza <jasmcaus@gmail.com>
2021-09-22 18:02:22 +0530

    Replace pattern `parse_*` with `ast_parse_*`

--- adorad/compiler/old_parser.c
+++ adorad/compiler/old_parser.c
@@ -1045,7 +1045,7 @@ static AstNode* ast_parse_match_case_kwd(Parser* parser) {
 // MatchItem
 //      Expr (ELLIPSIS Expr)?
 static AstNode* ast_parse_match_item(Parser* parser) {
-    AstNode* expr = parse_expr(parser);
+    AstNode* expr = ast_parse_expr(parser);
     if(expr == null)
         return null;
     
