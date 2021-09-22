d88fe8aa393217652d6f63c3b980d12275a86334
Jason Dsouza <jasmcaus@gmail.com>
2021-09-22 17:59:43 +0530

    Replace pattern `parse_*` with `ast_parse_*`

--- adorad/compiler/old_parser.c
+++ adorad/compiler/old_parser.c
@@ -628,7 +628,7 @@ static AstNode* ast_parse_comparison_expr(Parser* parser) {
 
 // BitwiseExpr
 //      BitShiftExpr (BitwiseOp BitShiftExpr)*
-static AstNode* parse_bitwise_expr(Parser* parser) {
+static AstNode* ast_parse_bitwise_expr(Parser* parser) {
     return parse_binary_op_expr(
         parser,
         BinaryOpChainInfinity,
