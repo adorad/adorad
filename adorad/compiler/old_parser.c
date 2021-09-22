6b2cbf3afd5151acaee8424bf7fef9b4893c16c1
Jason Dsouza <jasmcaus@gmail.com>
2021-09-22 15:11:53 +0530

    Replace pattern `ast_parse_*` with `parse_*`

--- adorad/compiler/old_parser.c
+++ adorad/compiler/old_parser.c
@@ -943,7 +943,7 @@ static Vec* parse_branch_list(Parser* parser, AstNode* (*list_parser)(Parser* pa
 
 // MatchExpr
 //      KEYWORD(match) LPAREN? Expr RPAREN? LBRACE MatchBranchList RBRACE
-static AstNode* ast_parse_match_expr(Parser* parser) {
+static AstNode* parse_match_expr(Parser* parser) {
     Token* match_token = parser_chomp_if(MATCH);
     if(match_token == null)
         return null;
