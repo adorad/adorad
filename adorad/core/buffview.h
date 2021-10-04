05ed2d19ec218042e944456c675191d0f7d07d47
Jason Dsouza <jasmcaus@gmail.com>
2021-10-04 10:31:41 +0530

    Add `core/buffview.h`

---
+++ adorad/core/buffview.h
@@ -0,0 +1,25 @@
+#ifndef CORETEN_BUFFVIEW_H
+#define CORETEN_BUFFVIEW_H
+
+typedef struct cstlBuffView cstlBuffView;
+typdef cstlBuffView BuffView
+
+struct cstlBuffView {
+    const char* data;    // view data (immutable)
+    UInt64 len;          // buffer size
+} cstlBuffView;
+
+#define BV(cstr_lit)   bv_from_parts(cstr_lit, sizeof(cstr_lit) - 1)
+
+// printf macros for BuffView
+// Usage:
+//     BuffView view = ...;
+//     printf("View: "BV_FMT"\n", BV_ARG(view))
+#define BV_FMT        "%.*s"
+#define BV_ARG(bv)    (int)(bv).len, (bv).data
+
+
+cstlBuffView bv_from_parts(const char* data, UInt64 len);
+cstlBuffView bv_from_cstr(const char* cstr);
+
+#endif // CORETEN_BUFFVIEW_H
