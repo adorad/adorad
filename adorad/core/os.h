efb55d9e9b52bb7abb71d614220f6d682599c4eb
Jason Dsouza <jasmcaus@gmail.com>
2022-03-24 19:52:35 +0530

    Test 2

--- adorad/core/os.h
+++ adorad/core/os.h
@@ -86,7 +86,9 @@ bool os_path_is_root(cstlBuffView* path);
             return path;
 
         cstlBuffView result = buffview_new(null);
+        printf("Original data = %s\n", path.data);
         char* end = buffview_end(&path);
+        printf("After data = %s\n\n\n", path.data);
 
         // Dirname
         if(!is_basename) {
